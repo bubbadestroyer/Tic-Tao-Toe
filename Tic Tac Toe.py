ed59bc60f2f163f38b86a61fd55db7c0deffedfc
Egor <godenzo@vk.com>
2022-07-22 14:56:40 +0300

    Added reset game function

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -11,62 +11,55 @@ def print_field():
         elif (i in (2, 4)):
             print('---|-----|----')
 
-def move(choice,mark):
-        game_field[game_field.index(choice)] = mark
-        print_field()
 
-def win():
-    if(game_field[0] == game_field[1] == game_field[2] or 
-           game_field[3] == game_field[4] == game_field[5] or 
-           game_field[6] == game_field[7] == game_field[8] or
-           game_field[0] == game_field[3] == game_field[6] or
-           game_field[1] == game_field[4] == game_field[7] or
-           game_field[2] == game_field[5] == game_field[8] or
-           game_field[0] == game_field[4] == game_field[8] or
-           game_field[2] == game_field[4] == game_field[6]):
-            print('Победил 1-ый игрок')
-            return True
 
-def player(mark):
+def win(id):
+    if (game_field[0] == game_field[1] == game_field[2]
+            or game_field[3] == game_field[4] == game_field[5]
+            or game_field[6] == game_field[7] == game_field[8]
+            or game_field[0] == game_field[3] == game_field[6]
+            or game_field[1] == game_field[4] == game_field[7]
+            or game_field[2] == game_field[5] == game_field[8]
+            or game_field[0] == game_field[4] == game_field[8]
+            or game_field[2] == game_field[4] == game_field[6]):
+        print(f'Победил игрок №{id}')
+        return True
+
+
+def player(id):
+    print(f'Ходит игрок №{id}')
     player_choice = int((input('Выберите номер ячейки: ')))
     if 1 <= player_choice <= 9:
-        move(player_choice, mark)
-        return win()
+        if id == 1:
+            game_field[game_field.index(player_choice)] = 'X'
+        else:
+            game_field[game_field.index(player_choice)] = 'O'
+        print_field()
+        return win(id)
     else:
         print('Введено некорректное значение, повторите ввод')
-        player()
+        player(id)
 
+def reset():
+    global game_field
+    answer = input(('Введите "Да", если желаете продожить игру'))
+    if answer.upper() == 'ДА':
+        game_field = [1, 4, 7, 2, 5, 8, 3, 6, 9]
+        game()
 
-
-
-# def first_player():
-#     player_choice = int((input('Выберите номер ячейки: ')))
-#     if 1 <= player_choice <= 9:
-#         move(player_choice, mark = 'X')
-#         return win()
-#     else:
-#         print('Введено некорректное значение, повторите ввод')
-#         first_player()
-
-# def second_player():
-#     player_choice = int((input('Выберите номер ячейки: ')))
-#     if 1 <= player_choice <= 9:
-#         move(player_choice, mark = 'O')
-#         win()
-#     else:
-#         print('Введено некорректное значение, повторите ввод')
-#         second_player()
 
 def game():
-    mark = 'X'
+    player_id = 1
     print_field()
     while True:
-        if player(mark) == True:
+        if player(player_id) == True:
             break
-        if mark == 'X':
-            mark = 'O'
+        if player_id == 1:
+            player_id = 2
         else:
-            mark = 'X'
-        
+            player_id = 1
+    reset()
+    
+
 
 game()
